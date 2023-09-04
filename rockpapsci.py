b56deae75210a5550ce8e7bec24f3f9af1b938d4
Onas55 <onas54@icloud.com>
2023-09-04 13:41:14 -0700

    Simple game

---
+++ rockpapsci.py
@@ -0,0 +1,34 @@
+import random
+
+def play_rock_paper_scissors():
+    print("Welcome to Rock, Paper, Scissors!")
+    choices = ["rock", "paper", "scissors"]
+
+    while True:
+        user_choice = input("Enter your choice (rock, paper, or scissors) or 'quit' to exit: ").lower()
+        if user_choice == "quit":
+            print("Thanks for playing. Goodbye!")
+            break
+
+        if user_choice not in choices:
+            print("Invalid choice. Please choose 'rock', 'paper', or 'scissors'.")
+            continue
+
+        computer_choice = random.choice(choices)
+
+        print(f"You chose {user_choice}.")
+        print(f"Computer chose {computer_choice}.")
+
+        if user_choice == computer_choice:
+            print("It's a tie!")
+        elif (
+            (user_choice == "rock" and computer_choice == "scissors") or
+            (user_choice == "scissors" and computer_choice == "paper") or
+            (user_choice == "paper" and computer_choice == "rock")
+        ):
+            print("You win!")
+        else:
+            print("Computer wins!")
+
+# if _name_ == "_main_":
+play_rock_paper_scissors()
